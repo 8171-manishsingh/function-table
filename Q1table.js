c9862bbc6390872dd99db628e242ad36a6fbbf1c
Manish Das <manishlakhaipur1999@gmail.com>
2025-02-18 00:20:32 +0530

    Add files via upload

---
+++ Q1table.js
@@ -0,0 +1,13 @@
+function num()
+{
+    let a =prompt("enter the number :")
+   for(let i=1;i<=10;i++)
+   {
+   
+       document.write(a+"*"+i+" = "+a*i+"<br>")
+          
+    }
+    document.write("<br>")
+
+}
+num()
---
+++ Q2table.js
@@ -0,0 +1,16 @@
+function num()
+{
+    let a =prompt("enter the number :")
+   for(let i=1;i<=a;i++)
+   {
+    for(let j=1;j<=10;j++)
+    {
+       document.write(i+"*"+j+"="+j*i+"<br>")
+          
+    }
+    document.write("</n>"+"<br>")
+
+   } 
+}
+
+num()
